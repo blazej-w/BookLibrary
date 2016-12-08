39a01874e1461aefaf3b37eb077c3119887cf7d9
blazej-w <blazej.wenderlich@gmail.com>
2016-12-08 14:30:03 +0100

    generated the views and controllers using crud

---
+++ src/BookBundle/Form/BookType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('description')->add('rating')->add('author')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BookBundle\Entity\Book'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'bookbundle_book';
+    }
+
+
+}
